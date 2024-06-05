6b72f2a32a16420006ad723c58bd8ba8fdcea164
pokem <pokemon.26rus@maik.ru>
2024-06-05 18:54:12 +0700

    Added(models):Реализовано создание моделей БД и выполнена синхронизация.

--- StudentPerformance/StudentPerformanceApp/admin.py
+++ StudentPerformance/StudentPerformanceApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from StudentPerformanceApp.models import Student
+from StudentPerformanceApp.models import Subject
+from StudentPerformanceApp.models import Grade
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Grade)
--- StudentPerformance/StudentPerformanceApp/models.py
+++ StudentPerformance/StudentPerformanceApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=False, null=False)
+    patronymic = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.patronymic}"
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    teacher = models.CharField(max_length=100, blank=False, null=False)
+    hours = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.student} - {self.subject} - {self.grade}"
