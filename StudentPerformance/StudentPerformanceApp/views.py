7a6206f959d02c1cc268befcdb818ea6f53bf819
pokem <pokemon.26rus@maik.ru>
2024-06-07 17:14:42 +0700

    Added(web_app):Реализован вывод данных из БД на страницу .

--- StudentPerformance/StudentPerformanceApp/views.py
+++ StudentPerformance/StudentPerformanceApp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Student, Subject, Grade
 
 def index_page(request):
-    return render(request, 'index.html')
+    students = Student.objects.all()
+    subjects = Subject.objects.all()
+    grades = Grade.objects.all()
+    return render(request, 'index.html', {
+        'students': students,
+        'subjects': subjects,
+        'grades': grades
+    })
